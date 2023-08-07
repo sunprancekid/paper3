41dc0b176cc02a897e996440884c77e5eb9f96b7
Matthew Dorsey <madorse2@ncsu.edu>
2023-08-07 07:57:16 -0400

    Added initial script for generating conH jobs on CHTC.

---
+++ data/simbin/bash/conH.sh
@@ -0,0 +1,22 @@
+# !/bin/bash
+set -e
+
+## Matthew Dorsey
+## 2023.08.07
+## script for generating conH jobs with polsqu2x2 module on CHTC linux systems
+
+
+## PARAMETERS
+# none
+
+
+## FUNCTIONS
+# none
+
+
+## OPTIONS
+# none
+
+
+## SCRIPT
+# none
