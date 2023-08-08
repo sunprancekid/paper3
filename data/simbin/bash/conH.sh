ae7519c7fa50d9376e1cadc9b820ce70f35fa6c8
Matthew Dorsey <madorse2@ncsu.edu>
2023-08-08 08:17:31 -0400

    Added programs for generating simulation directories and sub-directories.

--- data/simbin/bash/conH.sh
+++ data/simbin/bash/conH.sh
@@ -39,6 +39,10 @@ declare -i ACHAI_MAX=100
 # integer amount to increment fraction of a-chirality squares by
 # between the maxmimum and the minimum
 declare -i ACHAI_INC=25
+# default number of simulation events, unless specified by user
+declare -i EVENTS=100000000
+# default fraction used to decrease simulation temperature
+declare -i FRAC=95
 # number of times that each simulation is repeated
 declare -i NUM_REPLICATES=3
 
@@ -55,18 +59,84 @@ help () {
 	echo -e " -o           | overwrite existing simulation files and directories corresponding to job"
 	echo -e " -j << ARG >> | specify job title (default is ${JOB})"
 	echo -e " -c << ARG >> | specify simulation cell size (default is ${CELL})"
+	echo -e " -e << ARG >> | specify simulation events (default is ${EVENTS})"
+	echo -e " -f << ARG >> | specify annealing fraction (default is ${FRAC})"
 }
 
 # script for generating files for annealing simulations
 gensim () {
 
-	# TODO :: implement script for generating annealing simulations
+	## PARAMETERS
+	# list of sub directories to generate inside the main directory
+	SUBDIR=( "mov" "save" "anneal" "out" "txt" "sub" "fortran" )
+
+
+	## ARGUMENTS
+	# none
+
+
+	## SCRIPT
+	# inform user
+	if [[ VERB_BOOL -eq 1 ]]; then 
+		echo -e "\nGenerating annealing simulation (${ANNEALID}) in: ${D}"
+	fi
+
+	# check if directory already exists
+	declare -i GEN_DIR=0
+	if [[ -d "${D}" ]]; then 
+		# if the directory exists
+		# check if the directory should be over written
+		if [[ OVERWRITE_BOOL -eq 1 ]]; then 
+			declare -i GEN_DIR=1
+			rm -r "${D}"
+			# inform user 
+			if [[ VERB_BOOL -eq 1 ]]; then
+				echo "Directory already exists. Overwriting .."
+			fi
+		else
+			# inform the user
+			if [[ VERB_BOOL -eq 1 ]]; then
+				echo "Directory already exists."
+			fi
+		fi
+	else
+		# if the directory does not exist
+		declare	-i GEN_DIR=1
+		# inform the user
+		if [[ VERB_BOOL -eq 1 ]]; then 
+			echo "Generating directory .."
+		fi
+	fi
+	# if it does not, generate subdirectories
+	# or, if overwrite is true, delete the directory
+	# re-generate everything
+	if [[ GEN_DIR -eq 1 ]]; then
+		mkdir -p $D
+
+		# generate subdirectories
+		for sd in "${SUBDIR[@]}"; do 
+			mkdir -p "${D}${sd}/"
+		done
+	fi
+
+	exit 0
+
+	# write the subdag to the directory
+
+	# write files to directory
+	# pre / post - script wrappers
+	# fortran files
+	# submission scripts
+
+	# add submission instructions to the subdag
+	# look for existing save files, if they have not been overwritten
+
 
 }
 
 ## OPTIONS
 # parse options
-while getopts "voj:c:" option; do 
+while getopts "voj:c:e:f:" option; do 
 	case $option in
 		v) # execute script verbosely
 			
@@ -86,13 +156,23 @@ while getopts "voj:c:" option; do
 			;;
 		c) # specify the cell size 
 
-			# parse the cell size from the flag
+			# parse the cell size from the flag arguments
 			declare -i CELL="${OPTARG}"
+			;;
+		e) # specify simulation events
+			
+			# parse the value from the flag arguments
+			declare -i EVENTS="${OPTARG}"
+			;;
+		f) # specify annealing fraction
+			
+			# parse the value from the flag arguments
+			declare -i FRAC="${OPTARG}"
 			;;
 		\?) # default if illegal argument specified
 			
 			echo -e "\nIllegal argument ${option} specified.\n"
-			help()
+			help
 			exit $NONZERO_EXITCODE
 	esac
 done
@@ -106,11 +186,15 @@ shift $((OPTIND-1))
 ## SCRIPT
 # establish initial directories
 D0=${JOB}
-D1=${SIM_MOD}_${CELL}
+D1=${SIM_MOD}_c${CELL}
 
 # establish DAGMAN files
 JOBID="${JOB}_${SIM_MOD}"
 DAG="${JOBID}.dag"
+if [[ -f DAG ]]; then
+	# if the file exists, remove it
+	rm $DAG
+fi
 
 # TODO :: incorperate dipole into simulation parameters
 
@@ -120,23 +204,23 @@ declare -i ACHAI=$ACHAI_MIN
 while [[  ACHAI -le ACHAI_MAX ]]; do
 
 	# establish the achirality directory
-	ACHAI_STRING=${printf '%03d' ${ACHAI}}
+	ACHAI_STRING=$(printf '%03d' ${ACHAI})
 	D2="a${ACHAI_STRING}"
 
 	# second simulation parameters is the external field strength
 	declare -i FIELD=$FIELD_MIN
-	while [[ FIELD -le FIELD_MIN ]]; do
+	while [[ FIELD -le FIELD_MAX ]]; do
 
 		# establish the field directory
-		FIELD_STRING=${printf '%02d' ${FIELD}}
+		FIELD_STRING=$(printf '%02d' ${FIELD})
 		D3="h${FIELD_STRING}"
 
 		# third simulation parameter is the system density
 		declare -i ETA=$ETA_MIN
-		while [[ ETA -le ETA_MIN ]]; do 
+		while [[ ETA -le ETA_MAX ]]; do 
 
 			# establish the density directory
-			ETA_STRING=${printf '%02d' ${ETA}}
+			ETA_STRING=$(printf '%02d' ${ETA})
 			D4="e${ETA_STRING}"
 
 			# establish directory path
@@ -147,7 +231,7 @@ while [[  ACHAI -le ACHAI_MAX ]]; do
 			SUBDAG="${ANNEALID}.spl"
 
 			# generate simulation files
-			gensim()
+			gensim
 
 			# add annealing simulation to subdag
 			echo "SPLICE ${ANNEALID} ${SUBDAG} DIR ${D}" >> $DAG
