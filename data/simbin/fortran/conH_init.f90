464d2df3d7e2f8538afa6386b9378ba559f60e73
Matthew Dorsey <madorse2@ncsu.edu>
2023-08-13 11:40:20 -0400

    Added initial conH_initialization program.

---
+++ data/simbin/fortran/conH_init.f90
@@ -0,0 +1,21 @@
+!! Matthew A Dorsey
+!! 2023.08.13
+!! PURPOSE :: program for initializing conH annealing simulations
+
+program conH_init
+    use polarizedsquaremodule
+    implicit none
+
+
+    !! parse the arguments passed to the program during compilation
+    ! job title
+    ! simulation title?
+    ! simulation area fraction
+    ! simulation field strength
+    ! cell size
+    ! chirality fraction
+    ! initial temperature
+    ! length of annealing simulation
+    ! annealing fractional reduction
+
+end program conH_init
