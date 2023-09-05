e9320b050e4fe00e7da624691c290dfd19f27132
Matthew Dorsey <madorse2@ncsu.edu>
2023-09-05 09:55:12 -0400

    Added initial program for updating data from conH simulations downloaded from CHTC node.

---
+++ data/simbin/python/conH_update.py
@@ -0,0 +1,28 @@
+# filename :: conH_update.py
+# author :: Matthew Dorsey (@sunprancekid)
+# date :: 2023-09-05
+# purpuse :: program that collects data from conH simulations
+#			 compliles them in a way they they can be analyzed
+#			 by the user and by other programs
+
+
+import sys, os
+import pandas as pd
+import numpy as np
+
+## PARAMETERS
+# none
+
+
+## FUNCTIONS
+# none
+
+
+## ARGUMENTS
+# path to directory containing simulation files
+anal_dir = sys.argv[1]
+
+
+## SCRIPT
+# none
+
